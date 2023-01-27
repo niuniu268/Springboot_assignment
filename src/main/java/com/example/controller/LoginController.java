84f7a513e40a19ec8b22bb03890dd6d99e6d10a8
Mac <louis.niuniu@gmail.com>
2023-01-27 09:43:15 +0100

    wrap up

--- src/main/java/com/example/controller/LoginController.java
+++ src/main/java/com/example/controller/LoginController.java
@@ -1,16 +1,52 @@
 package com.example.controller;
 
+import com.baomidou.mybatisplus.core.conditions.Wrapper;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.example.dao.InvoiceMapper;
+import com.example.domain.Invoice;
+import com.example.service.InvoiceService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
 
 import javax.websocket.server.PathParam;
+import java.io.PrintWriter;
+import java.util.function.Consumer;
 
 @RestController
 @RequestMapping("/login")
 public class LoginController {
+    @Autowired
+    private InvoiceMapper invoiceMapper;
     @GetMapping
-    public void checkInfo(@PathParam( "username" ) String username, @PathParam("password") String password){
+    public RedirectView checkInfo(@PathParam( "username" ) String username, @PathParam("password") String password){
 
-    }
+        System.out.println(username );
+
+        System.out.println(invoiceMapper.selectList( null ) );
+
+        QueryWrapper <Invoice> wrapper = new QueryWrapper <>( );
+        wrapper.like( "username", username ).and( Consumer -> Consumer.like("password", password) );
+
+        System.out.println(invoiceMapper.selectCount( wrapper ) );
+
+        if(invoiceMapper.selectCount( wrapper ) < 1) {
+
+            return new RedirectView( "Error.html");
+
+        }else{
+
+            return new RedirectView(  "Invoice.html");
+        }
+
+
+        }
+
+
+
 }
